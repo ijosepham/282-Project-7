fe217c4da62529a4369e99d64c9e5f01eaf6825c
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-04-26 21:30:46 -0700

    changed all my instances of current temps to pointers. added an if to check if list is empty before adding last. updated tostring so that it uses stringstream, allowing for any datatype to be converted to string

--- 282 Project 7/CircularlyLinkedList.h
+++ 282 Project 7/CircularlyLinkedList.h
@@ -3,6 +3,7 @@
 
 #pragma once
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -17,14 +18,14 @@ template <typename T>
 class CircularlyLinkedList
 {
 private:
-	Node* head;
-	Node* tail;
-	int size;
+	Node <T>* head;
+	Node <T>* tail;
+	int length;
 public:
 	CircularlyLinkedList() {
 		head = NULL;
 		tail = NULL;
-		size = 0;
+		length = 0;
 	}
 
 	~CircularlyLinkedList() {
@@ -32,11 +33,11 @@ public:
 	}
 
 	int size() {
-		return size;
+		return length;
 	}
 
 	bool isEmpty() {
-		if (size == 0) {
+		if (length == 0) {
 			return true;
 		}
 		return false;
@@ -52,7 +53,7 @@ public:
 
 	void addFirst(T data) {
 		// create new temp node
-		Node <T> temp = new Node <T>;
+		Node <T>* temp = new Node <T>;
 
 		// assign the given data
 		temp->data = data;
@@ -62,11 +63,19 @@ public:
 
 		// change the head to the current node
 		head = temp;
+
+		// increase size
+		length++;
+
+		// if size is now 1, tail is also head
+		if (length == 1) {
+			tail = head;
+		}
 	}
 
 	void addLast(T data) {
 		// create new temp node
-		Node <T> temp = new Node <T>;
+		Node <T>* temp = new Node <T>;
 
 		// assign the given data
 		temp->data = data;
@@ -75,11 +84,21 @@ public:
 		// since its the end of the list
 		temp->next = NULL;
 
-		// insert the node at the end of the list
-		tail->next = temp;
+		// if the list is empty
+		if (length == 0) {
+			head = temp;
+			tail = temp;
+		}
+		else { // list isnt empty
+			// insert the node at the end of the list
+			tail->next = temp;
 
-		// update last node
-		tail = temp;
+			// update last node
+			tail = temp;
+		} 
+
+		// increase size
+		length++;
 	}
 
 	T removeFirst() {
@@ -88,6 +107,9 @@ public:
 
 		// replace the first node with the second one
 		head = head->next;
+
+		// decrease size
+		length--;
 	}
 
 	T removeLast() {
@@ -97,7 +119,8 @@ public:
 		// remove the last node in the list
 		
 		// create temp current var
-		Node <T> current = head;
+		Node <T>* current = head;
+
 		// iterate to the second to last node
 		for (int i = 0; i < size - 1; i++) {
 			current = current.next;
@@ -108,6 +131,9 @@ public:
 
 		// update tail
 		tail = current;
+
+		// decrease size
+		length--;
 	}
 
 	void display() {
@@ -115,22 +141,37 @@ public:
 	}
 
 	string toString() {
+		// any datatype to string
+		stringstream stream;
+		// temp string of each element
+		string temp = "";
+		// entire tostring 
 		string str = "";
 
 		// temp node to look at data
-		Node <T> current = head;
+		Node <T>* current = head;
 
 		// get all contents besides the last one
-		for (int i = 0; i < size - 1; i++) {
+		for (int i = 0; i < length - 1; i++) {
 			// add the current data to the string
-			str += current->data + ", ";
+			stream << current->data << ", ";
+
+			// put the element string on temp
+			stream >> temp;
+			
+			// add the temp string to the entire tostirng
+			str += temp;
 
 			// go to the next node
 			current = current->next;
 		}
 		
 		// add the last data without a comma
-		str += current->data;
+		stream << current->data;
+
+		// add the last temp string to the tostring
+		stream >> temp;
+		str += temp;
 
 		// return the tostring
 		return str;
--- 282 Project 7/DoublyLinkedList.h
+++ 282 Project 7/DoublyLinkedList.h
@@ -7,25 +7,27 @@
 
 using namespace std;
 
+/**
 template <typename T>
 struct Node {
 	T data;
 	Node* next;
 	Node* prev;
 };
+*/
 
 template <typename T>
 class DoublyLinkedList
 {
 private:
-	Node* header;
-	Node* trailer;
-	int size;
+	Node <T>* header;
+	Node <T>* trailer;
+	int length;
 public:
 	DoublyLinkedList() {
 		header = NULL;
 		trailer = NULL;
-		size = 0;
+		length = 0;
 	}
 
 	~DoublyLinkedList() {
@@ -33,22 +35,22 @@ public:
 	}
 
 	int size() {
-		return size;
+		return length;
 	}
 
 	bool isEmpty() {
-		if (size == 0) {
+		if (length == 0) {
 			return true;
 		}
 		return false;
 	}
 
 	T first() {
-
+		return new T;
 	}
 
 	T last() {
-
+		return new T;
 	}
 	
 	void addFirst(T data) {
@@ -60,11 +62,11 @@ public:
 	}
 
 	T removeFirst() {
-
+		return new T;
 	}
 
 	T removeLast() {
-
+		return new T;
 	}
 
 	void display() {
@@ -72,22 +74,37 @@ public:
 	}
 
 	string toString() {
+		// any datatype to string
+		stringstream stream;
+		// temp string of each element
+		string temp = "";
+		// entire tostring 
 		string str = "";
 
 		// temp node to look at data
-		Node <T> current = head;
+		Node <T>* current = header;
 
 		// get all contents besides the last one
-		for (int i = 0; i < size - 1; i++) {
+		for (int i = 0; i < length - 1; i++) {
 			// add the current data to the string
-			str += current->data + ", ";
+			stream << current->data << ", ";
+
+			// put the element string on temp
+			stream >> temp;
+
+			// add the temp string to the entire tostirng
+			str += temp;
 
 			// go to the next node
 			current = current->next;
 		}
 
 		// add the last data without a comma
-		str += current->data;
+		stream << current->data;
+
+		// add the last temp string to the tostring
+		stream >> temp;
+		str += temp;
 
 		// return the tostring
 		return str;
